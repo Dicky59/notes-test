7d10c7abe5b23a0191973d5e5b8050839cf6decf
Dicky59 <pekkaarvolaine@gmail.com>
2025-04-05 23:19:41 +0300

    Initial commit

---
+++ notes-frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    const footerStyle = {
+      color: 'blue',
+      fontFamily: "Inter, sans-serif",
+      fontSize: 14,
+    }
+  
+    return (
+      <div style={footerStyle}>
+        <br />
+        <em>
+          Note app, Department of Computer Science, University of Helsinki 2025
+        </em>
+      </div>
+    )
+  }
+  
+  export default Footer
